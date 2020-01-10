b964c4cb923c48baa0b056e5547eefaef87884c8
Thomas Dietrich <t.dietrich95@outlook.de>
2020-01-11 00:11:38 +0100

    Removed old code

--- test.php
+++ test.php
@@ -1,92 +1,34 @@
 <?php
 
 $dbname = "finance";
-
 // Create connection
 $sql = new mysqli(ini_get("mysqli.default_host"), ini_get("mysqli.default_user"),ini_get("mysqli.default_pw"), $dbname);
 
 // Check connection
+/*
 if ($conn->connect_error)
 {
 	die("Connection failed: " . $conn->connect_error);
 }
-
-
-
-// mysqli_query($sql, "CALL GetAllCategories");
-
-
-//$sql->query("CALL GetAllCategories");
-
-
-//$result = $sql->query("CALL GetAllCategories");
-$result = $sql->query("SELECT * FROM categories");
-
-//echo json_encode($result);
-/*
-echo json_encode(($result->fetch_array(MYSQLI_ASSOC)));
-
-
-$result->close();
-$sql->close();
-return json_encode(($result->fetch_assoc(MYSQLI_ASSOC)));
 */
 
+//Call Query
+$result = $sql->query("CALL GetAllCategories");
 
-// run the query
-// $result = mysql_query("SELECT * FROM table WHERE field = '{$q}'");
-
-// fetch all results into an array
+//Save response in Array
 $response = array();
 while($row = $result->fetch_array(MYSQLI_ASSOC))
 {
 	$response[] = $row;
 }
 
-
+//Echo to Javyscript
 header('Content-type: application/json');
 echo json_encode($response);
 
-// save the JSON encoded array
-//$jsonData = json_encode($response);
+//Close connections
 $result->close();
 $sql->close();
 exit();
-
-
-
-
-/*
-
-$result = $sql->query($query);
-if (!$result) {
-  printf("Query failed: %s\n", $mysqli->error);
-  exit;
-}
-
-while($row = $result->fetch_row()) {
-  $rows[]=$row;
-}
-
-$result->close();
-$sql->close();
-return $rows;
-*/
-/*
-//loop the result set
-echo "SQL-TEST";
-echo "<br>";
-while ($row = mysqli_fetch_row($result)){
-    $length = count($row);
-    $count = $length;
-    $length = 0;
-    while($length < $count)
-    {
-        echo $row[$length]. "    ";
-        $length = $length +1;
-    }
-    echo "<br>";
-}
-*/
 
 ?>
