71f2d251b21729c697dee9283445aea8b5ab4ad6
Thomas Dietrich <t.dietrich95@outlook.de>
2020-01-10 01:03:25 +0100

    Initial commit

---
+++ js/importHTML.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+    $("#Navbar_id").load("/html/Navs/Navbar_1.html");
+    $("#Import_Animationen").load("/html/Cards/Animationen.html");
+    $("#Import_Standbild").load("/html/Cards/Standbild.html");
+    $("#Import_Uhr").load("/html/Cards/Uhr.html");
+    $("#Import_Snake").load("/html/Cards/Snake.html");
+});
+
+
