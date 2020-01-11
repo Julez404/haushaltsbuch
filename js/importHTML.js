26651a50bd18092dfae22a48edb5eb095e0f693f
Thomas Dietrich <t.dietrich95@outlook.de>
2020-01-11 23:52:21 +0100

    Refactoring

--- js/importHTML.js
+++ js/importHTML.js
@@ -1,10 +1,15 @@
 
 $(document).ready(function () {
-    $("#Navbar_id").load("/html/Navs/Navbar_1.html");
-    $("#Import_Animationen").load("/html/Cards/Animationen.html");
-    $("#Import_Standbild").load("/html/Cards/Standbild.html");
-    $("#Import_Uhr").load("/html/Cards/Uhr.html");
-    $("#Import_Snake").load("/html/Cards/Snake.html");
+    $("#DIV_Buchung_MODAL").load("/html/Buchung/Modal.html");
+
+
+    $("#DIV_Buchung_NEU").load("/html/Buchung/Neu.html");
+    /*
+    $("#DIV_Buchung_PLANEN").load("/html/Buchung/Planen.html");
+    $("#DIV_Buchung_BEARBEITEN").load("/html/Buchung/Bearbeiten.html");
+
+    $("#DIV_Auswertung_NEU").load("/html/Auswertung/Neu.html");
+    $("#DIV_Auswertung_PLANEN").load("/html/Auswertung/Planen.html");
+    $("#DIV_Auswertung_BEARBEITEN").load("/html/Auswertung/Bearbeiten.html");
+    */
 });
-
-
