56afc5a0ca5346b8910935ce046f80c7fa09813e
Julian Linn <julian404@icloud.com>
2020-01-15 12:24:36 +0100

    Changed Name of return Value to TRANSACTION_VALUE

--- getTransmissionStructured.php
+++ getTransmissionStructured.php
@@ -57,7 +57,7 @@ else
 	{
 		$query = "SELECT $group_by, CATEGORY.CATEGORY_NAME, SUM(TRANSACTION_VALUE) AS 'TRANSACTION_VALUE' FROM $table";
 	}else {
-		$query = "SELECT $group_by, SUM(TRANSACTION_VALUE) FROM $table";
+		$query = "SELECT $group_by, SUM(TRANSACTION_VALUE) AS 'TRANSACTION_VALUE' FROM $table";
 	}
 }
 
